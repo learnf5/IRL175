c7f0e5a61c3619e228d938847210886fde54e88b
learnf5 <99698691+learnf5@users.noreply.github.com>
2025-05-25 22:02:02 -0400

    Update 10-ICR175.sh

--- 10-ICR175.sh
+++ 10-ICR175.sh
@@ -3,8 +3,8 @@ for i in {1..30}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Ac
 
 # download config script from GitHub, copy to bigip1 and run on bigip1
 curl --silent https://raw.githubusercontent.com/learnf5/irules/main/iRules_v17.1_ClassSetup.sh --output /tmp/iRules_v17.1_ClassSetup.sh
-sudo scp /tmp/iRules_v17.1_ClassSetup.sh 192.168.1.31:/shared/tmp/
-sudo ssh 192.168.1.31 source /shared/tmp/iRules_v17.1_ClassSetup.sh
+sudo scp /tmp/iRules_v17.5_ClassSetup.sh 192.168.1.31:/shared/tmp/
+sudo ssh 192.168.1.31 source /shared/tmp/iRules_v17.5_ClassSetup.sh
 
 # add snippets file to desktop
 curl --silent https://raw.githubusercontent.com/learnf5/irules/main/irules_sdt_17_lab_solutions.txt --output /home/student/Desktop/Lab_Solutions.txt
