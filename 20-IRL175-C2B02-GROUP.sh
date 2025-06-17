dd48b6cdbd803f6c0055aef89a62d65ab566d1ac
djlamey <76072289+djlamey@users.noreply.github.com>
2025-06-17 11:54:48 -0400

    Update 20-IRL175-C2B02-GROUP.sh
    updated GitHub path

--- 20-IRL175-C2B02-GROUP.sh
+++ 20-IRL175-C2B02-GROUP.sh
@@ -1,6 +1,6 @@
 # download file updates for TrainingServer3.7 from Github
-curl --silent https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/browserspecific.html.edge --output /tmp/browserspecific.html
-curl --silent https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/edge.png --output /tmp/edge.png
+curl --silent https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/files/browserspecific.html.edge --output /tmp/browserspecific.html
+curl --silent https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/files/edge.png --output /tmp/edge.png
 sudo scp /tmp/browserspecific.html 192.168.1.31:/shared/tmp
 sudo scp /tmp/edge.png 192.168.1.31:/shared/tmp
 sudo ssh 192.168.1.31 scp /shared/tmp/browserspecific.html 172.16.20.1:/var/www/server/1/
