018aa598139e6904cce3c9eb5f3dc863ed39de72
djlamey <76072289+djlamey@users.noreply.github.com>
2025-06-17 11:53:41 -0400

    Create IRL175-C2A02-IFILES.sh

---
+++ IRL175-C2A02-IFILES.sh
@@ -0,0 +1,2 @@
+# download file updates for TrainingServer3.7 from Github
+curl --silent https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/files/sorry.zip --output /home/student/Downloads/sorry.zip
